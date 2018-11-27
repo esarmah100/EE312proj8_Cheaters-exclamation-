a242382e4189c52b521905f2bfa044071dd7234f
esarmah100 <esarmah100@gmail.com>
2018-11-27 16:34:58 -0600

    First

---
+++ cmake-build-debug/CMakeFiles/EE312proj8_Cheaters_exclamation_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EE312proj8_Cheaters_exclamation_.dir/fileopen.cpp.obj"
+  "EE312proj8_Cheaters_exclamation_.pdb"
+  "EE312proj8_Cheaters_exclamation_.exe"
+  "EE312proj8_Cheaters_exclamation_.exe.manifest"
+  "libEE312proj8_Cheaters_exclamation_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EE312proj8_Cheaters_exclamation_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/EE312proj8_Cheaters_exclamation_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EE312proj8_Cheaters_exclamation_.
+# This may be replaced when dependencies are built.
